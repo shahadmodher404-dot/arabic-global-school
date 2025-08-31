6e410a02916d3c53a1f9ddbde59ed918a5734cc5
yousuf9o5 <yousuf1212121@gmail.com>
2025-08-31 22:29:54 +0300

    Update HomeHero component in home-hero.tsx

--- src/components/home/home-hero.tsx
+++ src/components/home/home-hero.tsx
@@ -32,7 +32,7 @@ function HomeHero() {
                 className="px-6 py-3 flex items-stretch gap-4 w-fit rounded-full mx-auto translate-y-1/2 relative z-20"
                 style={{ background: "linear-gradient(180deg, #FFFEED 0%, #FFFFFF 100%)" }}
             >
-                <CustomLink shadow="default" href={"/about"} size={"lg"} className="text-natural-primary px-3 py-2 border rounded-full">
+                <CustomLink shadow="default" href={"/about"} className="text-natural-primary px-8 py-2 border rounded-full">
                     {t("registration")}
                 </CustomLink>
                 <CustomLink href={"/contact"} shadow={"none"} variant={"outline"} className="text-natural-primary px-6 py-3 border rounded-full">
