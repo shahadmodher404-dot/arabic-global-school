6c5f1a31965cdcb76b84eb37ca691f488e28ad29
yousuf9o5 <yousuf1212121@gmail.com>
2025-09-04 16:07:46 +0300

    refactor: streamline ServicesFacilities and ServicesHero components for improved readability and structure

--- src/components/services/services-facilities.tsx
+++ src/components/services/services-facilities.tsx
@@ -15,43 +15,38 @@ export default function ServicesFacilities() {
     }));
 
     return (
-        <Section size={"screen"}>
-            <Section.Inner className="space-y-4 md:space-y-8">
-                <h1 className="section-header text-center md:text-start">{t("title")}</h1>
+        <Section className="space-y-4 md:space-y-8">
+            <h1 className="section-header text-center md:text-start">{t("title")}</h1>
 
-                <div className="flex items-center justify-between">
-                    <p className="section-description text-content-natural-primary/50 max-w-[530px]">{t("subtitle")}</p>
+            <div className="flex items-center justify-between">
+                <p className="section-description text-content-natural-primary/50 max-w-[530px]">{t("subtitle")}</p>
 
-                    <CustomLink shadow={"default"} href="">
-                        {t("register")}
-                    </CustomLink>
-                </div>
+                <CustomLink shadow={"default"} href="">
+                    {t("register")}
+                </CustomLink>
+            </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {
-                        /* Cards Grid */
-                        cards.map((card, index) => (
-                            <article
-                                key={card.title + index}
-                                className="flex flex-col bg-[#F2F6FC] rounded-[40px]"
-                            >
-                                <Image
-                                    width={422}
-                                    height={312}
-                                    src={card.imageSrc}
-                                    alt={card.title}
-                                    className="w-full object-cover object-center h-[321px] rounded-[38px] flex-shrink-0 p-2"
-                                />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    /* Cards Grid */
+                    cards.map((card, index) => (
+                        <article key={card.title + index} className="flex flex-col bg-[#F2F6FC] rounded-[40px]">
+                            <Image
+                                width={422}
+                                height={312}
+                                src={card.imageSrc}
+                                alt={card.title}
+                                className="w-full object-cover object-center h-[321px] rounded-[38px] flex-shrink-0 p-2"
+                            />
 
-                                <div className="text-center md:text-start p-6">
-                                    <h3 className="font-semibold text-2xl">{card.title}</h3>
-                                    <p className="text-content-natural-primary/50 md:text-lg">{card.description}</p>
-                                </div>
-                            </article>
-                        ))
-                    }
-                </div>
-            </Section.Inner>
+                            <div className="text-center md:text-start p-6">
+                                <h3 className="font-semibold text-2xl">{card.title}</h3>
+                                <p className="text-content-natural-primary/50 md:text-lg">{card.description}</p>
+                            </div>
+                        </article>
+                    ))
+                }
+            </div>
         </Section>
     );
 }
---
+++ src/components/services/services-safety.tsx
@@ -0,0 +1,5 @@
+import Section from "../ui/section";
+
+export default function ServicesSafety() {
+    return <Section ></Section>
+}
